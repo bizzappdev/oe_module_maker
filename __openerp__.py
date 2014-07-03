249cc78524274034fa840d3dcd0fded602ede8af
Ruchir Shukla <ruchir@bizzappdev.com>
2014-07-03 15:07:53 +0530

    fix bug and add description

--- __openerp__.py
+++ __openerp__.py
@@ -24,10 +24,28 @@
     "author": "Ruchir Shukla (Bizzappdev)",
     "website": "http://bizzappdev.com",
     "category": "GenericModules",
-    "depends": ["base", ],
+    "depends": ["base"],
     "summary": "OpenERP Module Maker",
     "description": """
-    """,
+OpenERP Module Maker/Generator
+==============================
+
+OpenERP/Odoo Module which help you to Create/Generate OpenERP Module.
+
+Just few configurations for Object, Fiels, Field type, views
+and it will generate module zip file for you.
+You can extract zip file put file in addons path and it is ready to install.
+
+Benifits
+--------
+
+* No Python knowledge is required
+* Hassle free OpenERP Module creation
+* All field types
+* Button Event
+* Inheriting object
+
+""",
     'images': [],
     "init_xml": [],
     "data": [
